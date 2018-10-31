9a8ffb8313b9e0332a8021e1fef6ee879ed93cd4
Nabilah Badriyah Gelshirani <bgelshirani@gmail.com>
2018-10-31 22:07:46 +0700

    tutorial7

--- tutorial7/src/main/java/com/apap/tutorial7/rest/Setting.java
+++ tutorial7/src/main/java/com/apap/tutorial7/rest/Setting.java
@@ -2,5 +2,5 @@ package com.apap.tutorial7.rest;
 
 public class Setting {
 	final public static String dealerUrl = "https://b3199a5f-bf37-4f66-b2df-98db4147b0bd.mock.pstmn.io";
-	final public static String carQueryUrl = "http://www.carqueryapi.com/documentation/api-usage/";
+	final public static String carQueryUrl = "https://www.carqueryapi.com/api/0.3";
 } 
